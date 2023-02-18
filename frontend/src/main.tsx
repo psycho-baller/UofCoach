44f7da8d74f0c305df51b57a3168424dcd0eea75
Rami Maalouf <rami.maalouf@ibm.com>
2023-02-18 13:46:47 -0700

    added react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 
@@ -8,6 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
