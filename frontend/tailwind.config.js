9019e3f2f36d6693dfecb266f2c9a35e5138cfc6
Rami Maalouf <rami.maalouf@ibm.com>
2023-02-18 15:08:31 -0700

    added tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+
 module.exports = {
-  content: [],
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
