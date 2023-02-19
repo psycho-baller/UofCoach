79605a2d9e4dbceecd9726b405bd20abaaecf6f3
Carlos Andres <carlos.andres.montoro@gmail.com>
2023-02-19 02:50:42 -0700

    Error handler returns JSON message

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,17 @@
 from app import app
 from app.routes.auth import *
 from app.routes.users import *
+from app.constants import ERROR_MESSAGES
+
+from werkzeug.exceptions import HTTPException
+
+
+# Error handler for all routes
+@app.errorhandler(HTTPException)
+def errorHandler(error):
+	return {
+		"error": ERROR_MESSAGES[error.code] if error.code in ERROR_MESSAGES else error.description
+	}, error.code
 
 
 @app.route("/")
