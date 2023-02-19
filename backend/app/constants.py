dbe20fc5c7e76a88002c829d89e7de4cc8d03a1b
Carlos Andres <carlos.andres.montoro@gmail.com>
2023-02-18 21:16:42 -0700

    Python Constants

---
+++ backend/app/constants.py
@@ -0,0 +1,9 @@
+TIMEZONE = "Canada/Mountain"
+
+MAX_ITEMS_PER_PAGE = 50
+
+ERROR_MESSAGES = {
+	403: "You don't have permission to perform that action",
+	404: "Page not found",
+	500: "Server error"
+}
