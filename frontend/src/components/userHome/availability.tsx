c0b96e4a8e79ab6892b73730a75f3aa06b73b7c9
Rami Maalouf <rami.maalouf@ibm.com>
2023-02-19 10:59:17 -0700

    edited availability

--- frontend/src/components/userHome/availability.tsx
+++ frontend/src/components/userHome/availability.tsx
@@ -1,5 +1,11 @@
 import Layout from './layout';
 
 export default function Settings() {
-  return <Layout title="Edit Availability"> </Layout>;
+  return (
+    <Layout title="Edit Availability">
+      <div className="flex">
+        <h2 className="text-5xl font-bold mb-4 mx-auto ">Coming Soon</h2>
+      </div>
+    </Layout>
+  );
 }
