2403ba8cac8d72c9875333245f1a6b4636e0778d
Carlos Andres <carlos.andres.montoro@gmail.com>
2023-02-19 03:19:50 -0700

    enable CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_session import Session
+from flask_cors import CORS
 from flask_login import LoginManager
 
 from sqlalchemy.ext.declarative import DeclarativeMeta
@@ -29,6 +30,7 @@ migrate = Migrate(app, db)
 
 bcrypt = Bcrypt(app)
 session = Session(app)
+cors = CORS(app, supports_credentials=True)
 loginManager = LoginManager(app)
 
 #
