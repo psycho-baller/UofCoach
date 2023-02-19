5e2a35d98f6fb98793ddb2fa39dad89ca4d9528a
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-02-18 20:42:38 -0700

    Worked on welcome page + logo

--- frontend/src/components/welcome.tsx
+++ frontend/src/components/welcome.tsx
@@ -1,12 +1,9 @@
 import { CallToAction } from './CallToAction';
-import { Faqs } from './Faqs';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Hero } from './Hero';
-import { Pricing } from './Pricing';
 import { PrimaryFeatures } from './PrimaryFeatures';
 import { SecondaryFeatures } from './SecondaryFeatures';
-import { Testimonials } from './Testimonials';
 
 export default function Welcome() {
   return (
@@ -17,9 +14,6 @@ export default function Welcome() {
         <PrimaryFeatures />
         <SecondaryFeatures />
         <CallToAction />
-        <Testimonials />
-        <Pricing />
-        <Faqs />
       </main>
       <Footer />
     </>
