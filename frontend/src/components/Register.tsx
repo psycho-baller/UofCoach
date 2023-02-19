6bdc2b5e138d44a551c1f255f206511425ad1e97
Carlos Andres <carlos.andres.montoro@gmail.com>
2023-02-19 10:32:43 -0700

    Connect auth forms to the backend

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -2,155 +2,162 @@ import { userInfo } from 'os'
 import { Logo } from './Logo'
 
 export default function Register() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const formData = new FormData(event.target);
-    //     const data = Object.fromEntries(formData.entries());
-    //     fetch('/api/register', {
-    //     method: 'POST',
-    //     headers: {
-    //     'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data)
-    //     })
-    //     .then(response => {
-    //     // handle response
-    //     })
-    //     .catch(error => {
-    //     // handle error
-    //     });
-    // }
+    // Get data into userInfo
+    const userInfo = {
+      username: e.currentTarget.username.value,
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+      fname: e.currentTarget.firstname.value,
+      lname: e.currentTarget.lastname.value,
+      phone: e.currentTarget.phone.value
+    }
 
-    return (
-      <>
-        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <div className='flex justify-center'>
-                <Logo className="w-40"/>
-            </div>
+    // Make a post request to the backend with the user info using fetch
+    fetch('http://localhost:5000/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        window.location.replace('/dashboard')
+      })
+  }
 
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register an account</h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-            </p>
+  return (
+    <>
+      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <div className='flex justify-center'>
+            <Logo className="w-40" />
           </div>
-  
-          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
-                  <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-                    Username*
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="username"
-                      name="username"
-                      type="username"
-                      autoComplete="username"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c] sm:text-sm"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                    Email*
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="current-email"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
-                    />
-                  </div>
-                </div>
 
-                <div>
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                    Password*
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
-                    />
-                  </div>
-                </div>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register an account</h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+          </p>
+        </div>
 
-                <div>
-                  <label htmlFor="firstname" className="block text-sm font-medium text-gray-700">
-                    First name*
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="firstname"
-                      name="firstname"
-                      type="firstname"
-                      autoComplete="firstname"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
-                    />
-                  </div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                  Username*
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="username"
+                    name="username"
+                    type="username"
+                    autoComplete="username"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c] sm:text-sm"
+                  />
                 </div>
+              </div>
 
-                <div>
-                  <label htmlFor="lastname" className="block text-sm font-medium text-gray-700">
-                    Last name
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="lastname"
-                      name="lastname"
-                      type="lastname"
-                      autoComplete="lastname"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
-                    />
-                  </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Email*
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="current-email"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
+                  />
                 </div>
+              </div>
 
-                <div>
-                  <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                    Phone number
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="phone"
-                      name="phone"
-                      type="phone"
-                      autoComplete="phone"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
-                    />
-                  </div>
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                  Password*
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
+                  />
                 </div>
-  
+              </div>
 
-  
-                <div>
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md border border-transparent bg-[#d6001c] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Register
-                  </button>
+              <div>
+                <label htmlFor="firstname" className="block text-sm font-medium text-gray-700">
+                  First name*
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="firstname"
+                    name="firstname"
+                    type="firstname"
+                    autoComplete="firstname"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
+                  />
                 </div>
-              </form>
-  
+              </div>
 
-            </div>
+              <div>
+                <label htmlFor="lastname" className="block text-sm font-medium text-gray-700">
+                  Last name
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="lastname"
+                    name="lastname"
+                    type="lastname"
+                    autoComplete="lastname"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+                  Phone number
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="phone"
+                    name="phone"
+                    type="phone"
+                    autoComplete="phone"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-[#d6001c] focus:outline-none focus:ring-[#d6001c]0 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md border border-transparent bg-[#d6001c] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+
+
           </div>
         </div>
-      </>
-    )
-  }
-  
+      </div>
+    </>
+  )
+}
