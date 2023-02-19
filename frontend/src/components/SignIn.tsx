c80a5ce83568045b88881d59825668bb6d2edcbf
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-02-19 07:55:55 -0700

    removed post from SignIn

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -22,7 +22,7 @@ export default function SignIn() {
   
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-              <form className="space-y-6" action="/dashboard" method="POST">
+              <form className="space-y-6" action="/dashboard">
                 <div>
                   <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                     Username
